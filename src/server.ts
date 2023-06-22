683d620c8e200f5f08b6147c92304dbf75802014
SalvatusX5 Dev <salvatusjoffrey@gmail.com>
2023-06-22 17:37:07 +0800

    updated

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import compression from 'compression';
 import DB from '../config/connectionDB';
 import router from './routes';
+import path from 'path';
 
 dotenv.config();
 DB();
@@ -17,6 +18,12 @@ app.use(compression());
 app.use(express.json());
 
 app.use('/api/v1/user', router());
+//static files
+app.use(express.static(path.join(__dirname, "./client/dist")));
+
+app.get("*", function (req: Request, res: Response) {
+    res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+});
 
 const PORT = process.env.PORT || 8080
 //Listen
